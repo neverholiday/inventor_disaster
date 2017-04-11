ab17450b21482e428a7296689422b155b91a7c30
neverholiday <super_nasrun@windowslive.com>
2017-04-11 21:56:19 +0700

    V1 play and pause

--- omx.py
+++ omx.py
@@ -1,18 +1,41 @@
 from omxplayer import OMXPlayer
 from time import sleep
 
-file_path_or_url = 'home/pi/Downloads/test.mp4'
+state1 = 0
+state2 = 0
+file_path_or_url = '/home/pi/Downloads/test.mp4'
 
-# This will start an `omxplayer` process, this might
-# fail the first time you run it, currently in the
-# process of fixing this though.
+#state 1
+#welcome home 
+#-------------------------------------------
+#p-Play
+#-------------------------------------------
+
+print "Welcome to OMXPlayer na ja"
+print "Press p and enter to start"
+
+while(state1 == 0):
+	input1 = raw_input("where is p : ")
+	if(input1 == 'p'):
+		state1 = 1
+		print "Let start."
+	else:
+		print "Please try again."
+
+#state 2
+#Play video and control video
+#p-Play,Pause
+#q-Quit
+
 player = OMXPlayer(file_path_or_url)
 
-# The player will initially be paused
+while(state2==0):
+	input2 = raw_input("Play|Pause|Quit : ")
+	if(input2=='p'):
+		player.play_pause()
+	elif(input2=='q'):
+		state2 = 1
+		player.quit()
 
-player.play()
-sleep(5)
-player.pause()
 
-# Kill the `omxplayer` process gracefully.
-player.quit()
+print "see you again"
